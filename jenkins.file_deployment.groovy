1eecbda76705e9cf94a64c05089baf847a48f765
Sree903012 <sree903012@gmail.com>
2021-11-06 22:07:14 +0530

    update

--- jenkins.file_deployment.groovy
+++ jenkins.file_deployment.groovy
@@ -7,7 +7,7 @@ agent any
             steps {
 
                 aws s3 cp s3://sree9030/buildacode/4/hello-4.war /tmp/
-                    cp /tmp/hello-4.war /var/lib/tomcat/webapps
+                  sh  "cp /tmp/hello-4.war /var/lib/tomcat/webapps"
 
 
             scp -i /tmp/sree.pem deployment.sh ec2-user@172.31.81.105:/tmp/
